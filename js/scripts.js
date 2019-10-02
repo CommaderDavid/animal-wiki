0cb80ba244d2024e8675de071611f9946487c33e
David Boedigheimer <stealth259@gmail.com>
2019-10-02 13:34:16 -0700

    Add if statement for form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
   $('#formOne').submit(function(event) {
     var animal = $("input:radio[name=animal]:checked").val();
-    console.log(animal);
+
+    if (animal === "turtle"){
+      $('#turtle').show();
+      $('#shark').hide();
+      $('#cuttlefish').hide();
+    } else if (animal === "shark") {
+      $('#turtle').hide();
+      $('#shark').show();
+      $('#cuttlefish').hide();
+    } else if (animal === "cuttlefish") {
+      $('#turtle').hide();
+      $('#shark').hide();
+      $('#cuttlefish').show();
+    }
+
     event.preventDefault();
   });
 });
